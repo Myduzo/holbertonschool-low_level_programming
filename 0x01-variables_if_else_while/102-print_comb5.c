100495abcdacfb6eeed8a77cf982c71c76282228
Myduzo <sahli.youssef@outlook.com>
2020-02-07 22:07:03 +0100

    .C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
  *  main - Entry point
  *
@@ -8,7 +8,7 @@ int main(void)
 {
 int i,j;
 for (i = 0; i < 99; i++)
-for (j = 0; j < 100; j++)
+for (j = 0; j <= 99; j++)
 {
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
