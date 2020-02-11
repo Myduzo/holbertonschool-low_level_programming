c18685ecf834d43e37881ccb9681d4948dabf91a
Myduzo <sahli.youssef@outlook.com>
2020-02-11 10:44:06 +0100

    .C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 char X;
 for (X = 'a'; X <= 'z'; X++)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 char X;
 int i;
