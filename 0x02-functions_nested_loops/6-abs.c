ea83cdbd02955b5421e5b7cc9690323de39a9be8
Myduzo <sahli.youssef@outlook.com>
2020-02-11 13:59:22 +0100

    .C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @i : int
+ * Return: Always 0.
+ */
+int _abs(int i)
+{
+if (i > 0)
+{
+return (i);
+}
+else
+{
+return (i * (-1));
+}
+}
