da1ac507dd277db950cb56d91a28af83b6ec1527
Myduzo <sahli.youssef@outlook.com>
2020-02-11 14:06:26 +0100

    .C

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @n : int
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
