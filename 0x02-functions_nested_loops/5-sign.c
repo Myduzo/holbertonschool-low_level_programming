0dbd24216cb6dfaeae0fdd0e1f7bc39cfc1a1132
Myduzo <sahli.youssef@outlook.com>
2020-02-11 13:39:19 +0100

    .C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - check the code for Holberton School students.
+ * @n : int
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
+}
