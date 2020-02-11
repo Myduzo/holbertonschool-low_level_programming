13f29aa30889fe88efe48342813acd320c7e6345
Myduzo <sahli.youssef@outlook.com>
2020-02-11 13:45:16 +0100

    .C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ if (n > 0)
 putchar('+');
 return (1);
 }
-else if (n == 0)
-{
-putchar('0');
-return (0);
-}
-else
+else if (n < 0)
 {
 putchar('-');
 return (-1);
+}
+else
+{
+putchar('0');
+return (0);
 }
 }
