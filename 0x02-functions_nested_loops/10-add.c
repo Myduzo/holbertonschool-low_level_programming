e42fd6742d2655cb9f44dd1fd2e2bd3a0b715f4f
Myduzo <sahli.youssef@outlook.com>
2020-02-11 17:47:52 +0100

    .C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @a : int
+ * @b : int
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
