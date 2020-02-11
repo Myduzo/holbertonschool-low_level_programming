45d29a2f65dea5896fd94b0e0be63ed8764e6c68
Myduzo <sahli.youssef@outlook.com>
2020-02-11 10:39:10 +0100

    .C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+int print_alphabet(void)
 {
 char X;
 for (X = 'a'; X <= 'z'; X++)
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main (void)
+int print_alphabet_x10(void)
 {
 char X;
 int i;
 for (i = 0; i <= 10; i++)
 for (X = 'a'; X <= 'z'; X++)
 {
-_putchar(X);  
-if(X == 'z')
+_putchar(X);
+if (X == 'z')
 _putchar('\n');
 }
 _putchar('\n');
