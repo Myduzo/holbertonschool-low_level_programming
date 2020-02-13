0856e72269c8518cac839e5d5e23d0d3dd354707
Myduzo <sahli.youssef@outlook.com>
2020-02-13 19:46:55 +0100

    .C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
 {
 printf("Fizz");
 }
@@ -22,7 +26,7 @@ else
 {
 printf("%d", i);
 }
-putchar (' ');
+printf(" ");
 }
-putchar ('\n');    
+printf("\n");
 }
