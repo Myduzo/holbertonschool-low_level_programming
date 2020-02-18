8059615f0d8bb892378fee59f0991390420e7ec0
Myduzo <sahli.youssef@outlook.com>
2020-02-18 14:26:08 +0100

    .C

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * rev_string - check the code for Holberton School students.
+ * @s : char
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+char q[20];
+int i = 0, j;
+while (s[i] != '\0')
+{
+q[i] = s[i];
+i++;
+}
+i--;
+for (j = i; j >= 0; j--)
+{
+s[i - j] = q[j]; 
+}
+}
