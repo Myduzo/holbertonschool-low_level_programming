4283723242e25ebbfb6a113c933e917044c51015
Myduzo <sahli.youssef@outlook.com>
2020-02-20 13:10:12 +0100

    .C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * strcat - check the code for Holberton School students.
+ * _strcat - check the code for Holberton School students.
  * @dest : char
  * @src : char
  * Return: Always 0.
@@ -10,7 +10,8 @@ char *_strcat(char *dest, char *src)
 int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 ;
-for (j = i; src[j] != '\0'; j++)	
-dest[i + j] = src [j];
+for (j = i; src[j] != '\0'; j++)
+dest[i + j] = src[j];
+dest[i + j] = '\0';
 return (dest);
 }
