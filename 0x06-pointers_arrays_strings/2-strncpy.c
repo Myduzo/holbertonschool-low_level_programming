4386dd7c38d7ee38acd05b9dd2a26428e64cdbbc
Myduzo <sahli.youssef@outlook.com>
2020-02-20 22:09:25 +0100

    .C

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-i
-	nt i;
-for (i = 0; src[i] != '\0'; i++)
+int i;
+for (i = 0; (i < n) && src[i] != '\0'; i++)
+{
 dest[i] = src[i];
-;
+}
+while (i < n)
+{
 dest[i] = '\0';
+i++;
+}
+return (dest);
 }
