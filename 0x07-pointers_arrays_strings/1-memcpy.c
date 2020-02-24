10e7be7d088924e9fbadb9803cfafdedd4db4e7f
Myduzo <sahli.youssef@outlook.com>
2020-02-24 13:35:21 +0100

    .C

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,11 +4,12 @@
  * @dest : char
  * @src : char
  * @n : unsigned int
+ * Return: Always 0.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
