ee736e6bcdbbd10252d5123cab4422ba96e39fe1
Myduzo <sahli.youssef@outlook.com>
2020-02-24 14:48:18 +0100

    .C

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * int_strspn - check the code for Holberton School students.
+ * @s : char
+ * @accept :char
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+
+}
