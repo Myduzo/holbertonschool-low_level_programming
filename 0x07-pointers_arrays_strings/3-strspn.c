b79d86c69a6d23bb391451749d38037d0ce69987
Myduzo <sahli.youssef@outlook.com>
2020-02-24 22:19:48 +0100

    .C

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,29 @@
 #include "holberton.h"
 /**
- * int_strspn - check the code for Holberton School students.
+ * _strspn - a function that gets the length of a prefix substring.
  * @s : char
  * @accept :char
  * Return: Always 0.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
-
+unsigned int i = 0, j =	0;
+while (s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+break;
+}
+j++;
+}
+if (accept[j] == '\0')
+{
+break;
+}
+j = 0;
+i++;
+}
+return (i);
 }
