5be9b59eafd9cc6f6e47ebdb155ebeb23d342c63
Myduzo <sahli.youssef@outlook.com>
2020-02-26 23:21:03 +0100

    .c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -11,10 +11,10 @@ if (*s1 == '\0' && *s2 == '\0')
 return (1);
 else if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-return (0);
 else if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
+else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
 else
 return (0);
 }
