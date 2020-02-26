5ccbb478d91262d4a0a329a46403a4e1f6dcc40f
Myduzo <sahli.youssef@outlook.com>
2020-02-26 22:02:14 +0100

    .C

--- 0x08-recursion/3-main.c
+++ 0x08-recursion/3-main.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int r;
+int r;
 
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+r = factorial(1);
+printf("%d\n", r);
+r = factorial(5);
+printf("%d\n", r);
+r = factorial(10);
+printf("%d\n", r);
+r = factorial(-1024);
+printf("%d\n", r);
+return (0);
 }
