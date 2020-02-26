bc1d273afba63591d86d775edd68f982fa225e25
Myduzo <sahli.youssef@outlook.com>
2020-02-26 23:05:53 +0100

    .C

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * wildcmp - a function that compares two strings.
+ * @s1: int
+ * @s2: int
+ * Return: Always 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+else if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
+else if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+else
+return (0);
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,28 @@
 #include "holberton.h"
 /**
-* _sqrt_recursion - a function that returns the natural square root of a number.
-* @n : int
+* sx - check the code for Holberton School students.
+* @i: int
+* @n: int
+* Return: Always 0.
+*/
+int sx(int i, int n)
+{
+if (i * i == n)
+return (i);
+else if (i * i > n)
+return (-1);
+else
+return (sx(i + 1, n));
+}
+/**
+* _sqrt_recursion - check the code for Holberton School students.
+* @n: int
 * Return: Always 0.
 */
 int _sqrt_recursion(int n)
 {
-  if (n == 0)
-  return (0);
-  if (n )
+if (n == 0)
+return (0);
+else
+return (sx(1, n));
 }
