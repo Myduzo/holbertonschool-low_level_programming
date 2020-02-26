d7422ae92d7b3a41b99cc0c93f024aa515b36a8b
Myduzo <sahli.youssef@outlook.com>
2020-02-26 21:55:47 +0100

    .C

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* _sqrt_recursion - a function that returns the natural square root of a number.
+* @n : int
+* Return: Always 0.
+*/
+int _sqrt_recursion(int n)
+{
+  if (n == 0)
+  return (0);
+  if (n )
+}
