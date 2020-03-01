1a56051b11d7d6e585fd67dd8d6b4bbd787cc539
Myduzo <sahli.youssef@outlook.com>
2020-03-01 16:11:48 +0100

    .C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
 int i, sum = 0;
 for (i = 1; i < argc; i++)
 {
-printf("%s\n", argv[i]);
-sum += atoi(argv[i]);
-if (sum != )
+if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+{
+printf("Error\n");
+return (1);
 }
+else
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
 return (0);
 }
