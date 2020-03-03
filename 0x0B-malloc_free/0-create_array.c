e1919c4f38640f03f2de1d19305bd58f8a806bfd
Myduzo <sahli.youssef@outlook.com>
2020-03-03 14:43:39 +0100

    .C

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - a function that creates an array of chars
+ * @c: the char of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i = 0;
+char *ar;
+ar = malloc(size * sizeof(char));
+if (ar == NULL)
+return (NULL);
+else
+{
+while (i <= size)
+{
+ar[i] = c;
+i++;
+}
+return (ar);
+}
+return (NULL);
+}
