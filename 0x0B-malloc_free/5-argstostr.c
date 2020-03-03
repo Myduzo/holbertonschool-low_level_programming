89d4615cc65a7c8a94a4b8baa15b91450c0581c5
Myduzo <sahli.youssef@outlook.com>
2020-03-03 22:32:30 +0100

    .C

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -7,3 +7,16 @@
 * Return: Always 0.
 */
 char *argstostr(int ac, char **av)
+{
+  int i, j;
+  if (ac == 0 || av == NULL)
+  return (NULL);
+for (i = 0; i < ac; i++)
+{
+
+}
+
+
+
+  return (NULL);
+}
