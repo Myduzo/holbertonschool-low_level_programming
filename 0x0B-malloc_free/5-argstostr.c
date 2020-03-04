435182aa07d18a697aac160f63d303ff87ede311
Myduzo <sahli.youssef@outlook.com>
2020-03-04 17:29:53 +0100

    .C

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,15 +8,31 @@
 */
 char *argstostr(int ac, char **av)
 {
-  int i, j;
-  if (ac == 0 || av == NULL)
-  return (NULL);
+int i, j, s, z;
+char *p;
+if (ac == 0 || av == NULL)
+return (NULL);
 for (i = 0; i < ac; i++)
 {
-
+for (j = 0; av[i][j]; j++)
+{
+s++;
 }
-
-
-
-  return (NULL);
+s++;
+}
+p = malloc(sizeof(char) * s);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+p[z] = av[i][j];
+z++;
+}
+p[z] = '\n';
+z++;
+}
+p[z] = '\0';
+return (p);
 }
