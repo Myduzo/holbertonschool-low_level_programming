53bb5924bad15b2ca475a3f141a9685c6c720de8
Myduzo <sahli.youssef@outlook.com>
2020-03-05 23:01:39 +0100

    .C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,13 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int *m;
-m = malloc(sizeof(unsigned int) * size);
+unsigned int i, *m;
+m = malloc(sizeof(unsigned int) * nmemb);
 if (!m)
 return (NULL);
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
+}
+for (i = 0; i < nmemb; i++)
+{
+m[i] = 0;
 }
 return (m);
 }
