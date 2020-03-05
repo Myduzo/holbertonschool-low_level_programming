0ab88430d2b2c9027e62df7a462d25b6c95d9368
Myduzo <sahli.youssef@outlook.com>
2020-03-05 12:48:09 +0100

    .C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - a function that concatenates two strings.
+ * @b : unsigned int
+ * Return: Always 0.
+ */
+ void *malloc_checked(unsigned int b)
+ {
+   unsigned int i, *m;
+   for (i = 0; i < b; i++)
+   ;
+   m = malloc(sizeof(unsigned int) * b);
+   if (m == NULL)
+   {
+   return (NULL);
+   }
+   free(m);
+   return (m);
+ }
