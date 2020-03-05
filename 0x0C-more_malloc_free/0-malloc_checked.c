07779a01621d5193ca939505e66f7acc4ab5208f
Myduzo <sahli.youssef@outlook.com>
2020-03-05 13:58:35 +0100

    .C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,16 @@
  * @b : unsigned int
  * Return: Always 0.
  */
- void *malloc_checked(unsigned int b)
- {
-   unsigned int i, *m;
-   for (i = 0; i < b; i++)
-   ;
-   m = malloc(sizeof(unsigned int) * b);
-   if (m == NULL)
-   {
-   return (NULL);
-   }
-   free(m);
-   return (m);
- }
+void *malloc_checked(unsigned int b)
+{
+unsigned int i, *m;
+for (i = 0; i < b; i++)
+;
+m = malloc(sizeof(unsigned int) * b);
+if (!m)
+{
+return (NULL);
+}
+free(m);
+return (m);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - a function that concatenates two strings.
+ * @n : unsigned int.
+ * @s1 : char.
+ * @s2 : char.
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j, x, y;
+char *s3;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] < s2[n]; j++)
+;
+s3 = malloc(sizeof(unsigned int) * n + 1);
+if (!s3)
+{
+return (NULL);
+}
+for (x = 0; x < i; x++)
+{
+s3[x] = s1[x];
+}
+for (y = 0; y < n; y++)
+{
+s3[x + y] = s2[y];
+}
+return (s3);
+}
