df9e847df4252e9f5bfed02fcfcceb5475660da6
Myduzo <sahli.youssef@outlook.com>
2020-03-06 10:35:32 +0100

    .C

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,18 +8,21 @@
  */
 int *array_range(int min, int max)
 {
-  int i, j, *m;
-  if (min > max)
-  return (NULL);
-  for (i = 0; i < min; i++)
-  ;
-  for (j = 0; j < max; j++)
-  ;
-  m = malloc((sizeof(int) * min) + (sizeof(int) * max));
-  if (!m)
-  {
-  return (NULL);
-  }
-  free(m);
-  return (m);
+int i, j, *m;
+if (min > max)
+return (NULL);
+for (i = 0; i < min; i++)
+;
+for (j = 0; j < max; j++)
+;
+m = malloc((sizeof(int) * min) + (sizeof(int) * max) + 1);
+if (!m)
+{
+return (NULL);
+}
+for (i = min; i <= max; i++)
+{
+m[i] = i;
+}
+return (m);
 }
