186c2b01fffa66fac06e5ad9b033e0c4bb631bf8
Myduzo <sahli.youssef@outlook.com>
2020-03-09 11:55:18 +0100

    preprossesor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * a program that prints the name of the file it was compiled from, followed by a new line.
+ */
+ int main(int argc, char **argv)
+ {
+   return (argv);
+ }
