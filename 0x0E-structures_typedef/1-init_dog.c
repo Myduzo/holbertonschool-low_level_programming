90bc1e86f81692743a747abfad58527305c4cf3f
Myduzo <sahli.youssef@outlook.com>
2020-03-10 08:57:29 +0100

    typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * int_dog - a function that initialize a variable of type struct dog.
  * @d : dog
  * @name : char
  * @age : float
  * @owner : char
- * Return: 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
