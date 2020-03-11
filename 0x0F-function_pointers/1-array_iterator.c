be000491bf3cedc467ed5ce164a7f2ca43f0bc49
Myduzo <sahli.youssef@outlook.com>
2020-03-11 21:28:42 +0100

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,15 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
-* struct size_t : def size.
-* @size : int.
-*/
-struct size_t
-{
-int size;
-}
-
-/**
 * print_name - a function that executes a function given.
 * @array : int.
 * @size : size_t.
@@ -18,8 +9,7 @@ int size;
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-for (i = 0; array[i] < size; i++)
-;
-action(array);
+size_t i;
+for (i = 0; i < size; i++)
+action(array[i]);
 }
