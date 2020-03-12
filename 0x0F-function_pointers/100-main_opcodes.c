550325f74ed7053dbee74be172935b228a6092af
Myduzo <sahli.youssef@outlook.com>
2020-03-12 22:53:22 +0100

    Function pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,13 +8,15 @@
 */
 void main (int argc, char *argv[])
 {
+int x;
+x = atoi(argv[2]);
 
-  if (argc != 2)
-  printf("Error\n");
-  exit(1);
+if (argc != 2)
+printf("Error\n");
+exit(1);
 
-  if (argv[1] < 0)
-  printf ("Error\n");
-  exit(2);
+if (argv[1] < 0)
+printf ("Error\n");
+exit(2);
 
 }
