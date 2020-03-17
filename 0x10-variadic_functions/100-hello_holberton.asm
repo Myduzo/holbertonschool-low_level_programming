c2800fa1ac4f9aa2b9bee585d372f59eaea4bb43
Myduzo <sahli.youssef@outlook.com>
2020-03-17 19:49:24 +0100

    0

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
-*
-*
-*
-*/
-int main()
-{
-char c[16] = "Hello, Holberton";
-int fd, wr;
-wr = write (fd, c, sizeof(c));
-return (0);
-}
+msg db "Hello, Holberton",0,10,13 ;print message
+len equ $-msg ;length of message
+
+
+global _begin
+_begin:
+mov eax,4
+mov ebx,1
+mov ecx,msg
+mov edx,len
+int 0x80
+
+mov eax,1
+mov ebx,0
+int 80h
