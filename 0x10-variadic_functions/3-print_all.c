d27bfce73e5aab7b00da678394b0d1d0d4159107
Myduzo <sahli.youssef@outlook.com>
2020-03-17 22:13:58 +0100

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+/**
+* _char - print char
+* @x : pointer
+*/
+void _char(va_list x)
+{
+printf("%c", va_arg(x, int));
+}
+
+/**
+* _int - print integer
+* @x : pointer
+*/
+void _int(va_list x)
+{
+printf("%i", va_arg(x, int));
+}
+
+/**
+* _float - print float
+* @x : pointer
+*/
+void _float(va_list x)
+{
+printf("%f", va_arg(x, double));
+}
+
+/**
+* _string - print string
+* @x : pointer
+*/
+void _string(va_list x)
+{
+char *string;
+string = va_arg(x, char *);
+if (string == NULL)
+string = "(nil)";
+printf("%s", string);
+}
+
 /**
 * print_all - a function that prints anything.
 * @format : char.
@@ -10,49 +51,27 @@ void print_all(const char * const format, ...)
 {
 va_list x;
 int i, j;
-
+i = 0;
+j = 0;
 lamis tab[] = {
   {"c", _char},
   {"i", _int},
   {"f", _float},
-  {"s", _string};
+  {"s", _string},
 };
 
 va_start(x, format);
 
-i = 0;
-x = 0;
+while (j < 4)
+{
 while (format[i] != '\0')
 {
-
-if (format[i] == tab[j].ch[0])
+if (format[i] == tab[j].ch)
 tab[j].f(x);
 i++;
 }
+j++;
+}
 va_end(x);
 printf("\n");
-}
-
-void _char(va_list x)
-{
-printf("%c", va_arg(x, int));
-}
-
-void _int(va_list x)
-{
-printf("%i", va_arg(x, int));
-}
-
-void _float(va_list x)
-{
-printf("%f", va_arg(x, double));
-}
-
-void _string(va_list x)
-{
-char *string;
-string = va_arg(x, char *);
-if (string == NULL)
-string = "(nil)";
-printf("%s", string);
 }
