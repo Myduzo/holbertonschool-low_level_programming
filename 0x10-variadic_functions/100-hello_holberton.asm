ff1c77697791b92d65be38de0d820713a93f3de6
Myduzo <sahli.youssef@outlook.com>
2020-03-17 20:04:57 +0100

    variadic functions

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,9 +1,12 @@
-msg db "Hello, Holberton",0,10,13 ;print message
-len equ $-msg ;length of message
+global _start
+section .data
+msg db "Hello, Holberton",0x0a ;print message
+len equ $ - msg ;length of message
 
+section .bss
 
-global _begin
-_begin:
+section .text
+_start:
 mov eax,4
 mov ebx,1
 mov ecx,msg
@@ -12,4 +15,4 @@ int 0x80
 
 mov eax,1
 mov ebx,0
-int 80h
+int 0x80
