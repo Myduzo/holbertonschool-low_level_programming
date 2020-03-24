3d722b34ab8ff138d92234034d7a6d480ab36c5b
Myduzo <sahli.youssef@outlook.com>
2020-03-24 13:30:41 +0100

    Linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,6 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-
 listint_t *new = malloc(sizeof(listint_t));
 if (new == NULL)
 return (NULL);
@@ -19,7 +18,6 @@ return (NULL);
 new->n = n;
 new->next = *head;
 *head = new;
-
 
 return (*head);
 }
