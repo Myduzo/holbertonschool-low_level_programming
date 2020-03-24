22b7771b13cbb4523bba7fdb63d6c431849aae29
Myduzo <sahli.youssef@outlook.com>
2020-03-24 19:54:05 +0100

    Linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,6 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new = malloc(sizeof(listint_t));
-
 if (new == NULL)
 return (NULL);
 
@@ -30,5 +29,5 @@ last = last->next;
 
 last->next = new;
 }
-return (*head);
+return (new);
 }
