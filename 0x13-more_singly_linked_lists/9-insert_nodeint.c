f8733331949d9e7e6839de0a17c6f6605f572ea0
Myduzo <sahli.youssef@outlook.com>
2020-03-24 21:18:07 +0100

    Linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,34 +12,39 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int x = 0;
 
-listint_t *node = *head;
+listint_t *temp = *head;
 
 listint_t *new = malloc(sizeof(listint_t));
-
-
-if (!head || !new)
+if (new == NULL)
 return (NULL);
 
-new->n = n;
-new->next = NULL;
-
-if (idx == 0)
-{	
-new->next = *head;
-*head = new;
-}
-
-
-while (node != NULL)
+while (temp && x < (idx - 1))
 {
-if (x == idx - 1)
-{
-new->next = node ->next;
-node->next = new;
-}
-node = node->next;
+temp = temp->next;
 x++;
 }
 
+if (temp != NULL)
+{
+	
+if (new != NULL)
+{	
+new->n = n;
+
+if (idx == 0)
+{
+new->next = *head;
+*head = new;	
+}
+
+else
+{
+new->next = temp->next;
+temp->next = new;
+}
+
+return (new);
+}
+}
 return (NULL);
 }
