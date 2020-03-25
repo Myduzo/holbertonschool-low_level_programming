1c887098df6295abc9aa7af5309133b9c609f40f
Myduzo <sahli.youssef@outlook.com>
2020-03-25 12:25:36 +0100

    Linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 unsigned int x = 0, count = 0;
 
 listint_t *temp = *head;
-listint_t *new = (listint_t *) malloc(sizeof(listint_t));
+listint_t *new = malloc(sizeof(listint_t *));
 if (new == NULL)
 return (NULL);
 
-if (*head == NULL && idx != 0 )
+if (*head == NULL && idx != 0)
 return (NULL);
 
 if (idx == 0)
