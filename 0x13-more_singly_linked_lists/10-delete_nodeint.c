d88f4efaac0495709c2be4b1959bfadb16ed2111
Myduzo <sahli.youssef@outlook.com>
2020-03-25 14:16:51 +0100

    Linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 unsigned int x = 0, count = 0;
 
 listint_t *temp = *head;
+listint_t *new;
 
 if (temp == NULL)
 return (-1);
@@ -39,8 +40,10 @@ temp = temp->next;
 x++;
 }
 
-temp->next = temp->next->next;
-free(temp->next);
+new = temp;
+temp = temp->next;
+new->next = temp->next;
+free(temp);
 
 return (1);
 }
