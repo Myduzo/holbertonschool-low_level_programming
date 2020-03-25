e8b9336da2e9311e684a93206fb83a9da74305b3
Myduzo <sahli.youssef@outlook.com>
2020-03-25 11:02:23 +0100

    Linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,15 +2,10 @@
 #include <stdio.h>
 /**
 * myStartup - a function that prints before main.
-*
+* 
 */
-void myStartup(void) __attribute__ ((constructor));
-/**
-* myStartup - a function that prints before main.
-*
-*/
-
 void myStartup(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
