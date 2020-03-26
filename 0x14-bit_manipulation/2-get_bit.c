e2e52c5e6e2b2385d3181b4baf577229831a9a87
Myduzo <sahli.youssef@outlook.com>
2020-03-26 20:44:07 +0100

    manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* get_bit - returns the value of a bit.
+* @n : decimal number
+* @index : bit
+* Return: value of a bit.
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int x = 0;
+if (!index && !n)
+return (-1);
+
+if (index > 0)
+x = n >> index;
+return (x & 1);
+}
