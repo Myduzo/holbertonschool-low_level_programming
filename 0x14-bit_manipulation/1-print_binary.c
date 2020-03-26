7df589f19d63e36b5b34278227476b7136f8b64e
Myduzo <sahli.youssef@outlook.com>
2020-03-26 19:53:02 +0100

    manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
-* print_binary - function
-* @n : 
-*
+* print_binary - convert from decimal to binary
+* @n : decimal number
 */
 void print_binary(unsigned long int n)
 {
 if (n > 1)
-print_binary (n >> 1);
+print_binary(n >> 1);
 _putchar ((n & 1) + '0');
 }
