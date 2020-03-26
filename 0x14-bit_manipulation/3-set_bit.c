6297c082faf0ff54863edd3e1d86ffb56e3df737
Myduzo <sahli.youssef@outlook.com>
2020-03-26 21:30:49 +0100

    manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,10 +7,9 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (!index && !n)
+if (index >= sizeof(*n) * 8)
 return (-1);
 
-if (index < sizeof(*n) * 8)
 *n |= 1 << index;
 
 return (1);
