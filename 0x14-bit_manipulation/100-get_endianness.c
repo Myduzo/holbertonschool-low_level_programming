094551ce60972629f0d968ba4751fbdea09bbbc1
Myduzo <sahli.youssef@outlook.com>
2020-03-27 10:59:20 +0100

    manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* get_endianness - checks the endianness.
+* Return: 0 - 1.
+*/
+int get_endianness(void)
+{
+unsigned int x = 1;
+char *c = (char *) & x;
+
+if (*c)
+return (1);
+
+else
+return (0); 
+}
