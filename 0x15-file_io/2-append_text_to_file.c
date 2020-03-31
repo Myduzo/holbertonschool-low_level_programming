68875044b5af0aec30c6c13a1b63455a3fd90d45
Myduzo <sahli.youssef@outlook.com>
2020-03-31 17:06:45 +0100

    FILE I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename : file name.
+* @text_content : text.
+* Return: 0.
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, x = 0, wt;
+
+fd = open(filename, O_WRONLY | O_APPEND, 0600);
+
+if (fd == -1)
+return (-1);
+
+if (filename == NULL)
+return (-1);
+
+if (text_content)
+{
+for (; text_content[x]; x++)
+;
+}
+
+else
+text_content = "";
+
+wt = write(fd, text_content, x);
+
+if (wt == -1)
+return (-1);
+
+close(fd);
+return (1);
+}
