e616adbeeb6010211113f9944cf4f04a760b1ef6
Myduzo <sahli.youssef@outlook.com>
2020-05-29 20:42:33 +0100

    creates a hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table
+ * @size - sizeof the array
+ * Return: function that creates a hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tab;
+
+	tab = malloc(sizeof(hash_table_t));
+	if (!tab)
+		return (NULL);
+
+	if (!size)
+		return (NULL);
+
+	tab->size = size;
+	tab->array = calloc(size, sizeof(hash_table_t));
+	if (!tab->array)
+	{
+		free(tab);
+		return (NULL);
+	}
+
+	free(tab->array);
+	free(tab);
+	return (tab);
+}
