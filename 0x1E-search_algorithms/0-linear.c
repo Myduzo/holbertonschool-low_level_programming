b9d2dfd8e9254a73f80dc4b788154f918468ffa7
Myduzo <sahli.youssef@outlook.com>
2020-08-26 13:11:02 +0100

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
  * @array : array of integers
  * @size : array's size
  * @value : the value to search for
- * Return : the first index where value is located
+ * Return: the first index where value is located
  */
 int linear_search(int *array, size_t size, int value)
 {
