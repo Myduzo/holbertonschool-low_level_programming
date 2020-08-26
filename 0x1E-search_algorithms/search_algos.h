3be0007aa9b64cb6c85a52e6a0e824c725ab73df
Myduzo <sahli.youssef@outlook.com>
2020-08-26 13:09:50 +0100

    library.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _LINEAR_H_
+#define _LINEAR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int left, int right);
+
+#endif
